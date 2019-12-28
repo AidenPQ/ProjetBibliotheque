599ce6906fdbc35812455fdea70bd280bce28ba4
Ngandjui Tiako <ngandjuitpq@gmail.com>
2019-12-28 17:17:37 +0100

    Update livre.h

--- livre.h
+++ livre.h
@@ -13,6 +13,25 @@ using namespace std;
 
 class Livre{
 protected:
+	string code_livre;
+	string auteur;
+	string titre;
+	string ISBN;
+	string publique;
+	bool etatEmprunt;
+
+public:
+	Livre(string, string, string, string, string = "");
+	Livre(const Livre);
+	string getCode_livre() const;
+	string getAuteur() const;
+	string getTitre() const;
+	string getPublique() const;
+
+	void setEtatEmprunt(bool);
+	void setCode_Livre(string);
+
+	void affiche();
 
 };
 
