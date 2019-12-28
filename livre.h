9cf18cb353676f34058d26f8bf85b1465df354a9
Ngandjui Tiako <ngandjuitpq@gmail.com>
2019-12-28 17:07:01 +0100

    Creation de livre.h

---
+++ livre.h
@@ -0,0 +1,22 @@
+/*
+ * livre.h
+ *
+ *  Created on: 28 déc. 2019
+ *      Author: ngand
+ */
+
+#ifndef LIVRE_H_
+#define LIVRE_H_
+
+#include <iostream>
+using namespace std;
+
+class Livre{
+protected:
+
+};
+
+
+
+
+#endif /* LIVRE_H_ */
